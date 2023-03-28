ea163a60974b16c1134505fb78cb66c7f0c938aa
hasancagrieren <129182596+hasancagrieren@users.noreply.github.com>
2023-03-28 14:08:37 +0100

    Print "Hello World"
    Added sout for "Hello World".

--- src/githubprime/GitHubPrime.java
+++ src/githubprime/GitHubPrime.java
@@ -14,7 +14,8 @@ public class GitHubPrime {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
+        
     }
     
 }
