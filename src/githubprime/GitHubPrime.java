f41b62ad0260af2b0c03e6a68e7888b2384c5e09
hasancagrieren <129182596+hasancagrieren@users.noreply.github.com>
2023-03-28 14:22:04 +0100

    Update GitHubPrime.java
    Add "Hello Classmates"

--- src/githubprime/GitHubPrime.java
+++ src/githubprime/GitHubPrime.java
@@ -15,6 +15,8 @@ public class GitHubPrime {
      */
     public static void main(String[] args) {
         System.out.println("Goodbye Moon!");
+        System.out.println("Hello Classmates");
+        
         
     }
     
